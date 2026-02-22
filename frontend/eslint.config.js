4ed22c5f9f95e2e2e954b6a620e642e720144547
MartinAban <martinalejandroabanmiranda@gmail.com>
2026-02-22 18:48:30 -0400

    fix: Eslint v3

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   {
@@ -24,14 +24,12 @@ export default defineConfig([
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]'}],
     },
-    overrides: [
-      {
-        files: ['**/__tests__/*.js', '**/*.test.js'],
-        env: {
-          jest: true,
-          node: true
-        }
-      }
-    ]
+  },
+  {
+    files: ['**/__tests__/*.js', '**/*.test.js'],
+    env: {
+      jest: true,
+      node: true
+    }
   }
 ])
