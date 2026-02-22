11dd3041bee52eb11cc5c303984570bbd3dd1910
MartinAban <martinalejandroabanmiranda@gmail.com>
2026-02-22 18:22:18 -0400

    Fix: arreglar los errores del Eslint

--- frontend/__tests__/health.test.js
+++ frontend/__tests__/health.test.js
@@ -1,3 +1,5 @@
+import { test, expect } from '@jest/globals';
+
 test('health check dummy', () => {
   expect(true).toBe(true);
 });
--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -5,7 +5,12 @@ export default defineConfig({
 
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      
+      on("before:run", () => {
+      
+      });
+      
+      void config;
     },
   },
 });
